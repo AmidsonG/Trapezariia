9275000bbfa99d384d468dfff1bbb9adab2e696f
AmidsonG <amidson10@gmail.com>
2024-11-26 12:34:09 +0100

    OUTRO DOM

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter1',
+  appId: 'ionic.starter',
   appName: 'Trapezaria',
   webDir: 'www',
   server: {
