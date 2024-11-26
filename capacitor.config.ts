2294c4fba9999a7529bbfb75cd0f87312579ae7b
AmidsonG <amidson10@gmail.com>
2024-11-26 12:47:14 +0100

    new domain

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'ionic.starter',
+  appId: 'io.ionic.starter1',
   appName: 'Trapezaria',
   webDir: 'www',
   server: {
