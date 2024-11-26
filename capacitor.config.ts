90e79b0f09239965f1652d82e3220d2528beb872
AmidsonG <amidson10@gmail.com>
2024-11-26 12:08:09 +0100

    novo dominio

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'rotasFeitas',
+  appId: 'io.ionic.starter1',
+  appName: 'Trapezaria',
   webDir: 'www',
   server: {
     androidScheme: 'https'
