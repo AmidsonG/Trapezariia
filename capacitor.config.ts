ac28f6eddde5ea10527b4c8b01712a37d59b3f9f
AmidsonG <amidson10@gmail.com>
2024-12-19 20:30:59 +0100

    update capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,16 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.trapezaria.app',
   appName: 'Trapezaria',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0, // Desativa o splash automático
+      backgroundColor: "#ffffff", // Cor de fundo, pode ser ajustada ou removida
+      androidSplashResourceName: "splash", // Recurso de splash opcional
+      androidScaleType: "CENTER_CROP", // Ajusta a escala
+      showSpinner: false // Remove o spinner de carregamento
+    }
+  }
 };
 
 export default config;
